241167fe850093c18a47ef9cc1e26c8298d570ce
aydanm97 <aydanm97@outlook.dk>
2022-02-21 14:13:28 +0100

    Øvelse 3-7 OOP

--- FagTilmeldingApp/Program.cs
+++ FagTilmeldingApp/Program.cs
@@ -1,1 +1,16 @@
-﻿// Iteration 1
+﻿using FagTilmeldingApp.Codes;
+// Iteration 1
+Semester se = new Semester("");
+Console.WriteLine("Angiv skole: ");
+string? skole = Console.ReadLine();
+Console.WriteLine("Angiv hovedforløb: ");
+string? hovedforløb = Console.ReadLine();
+Console.ReadKey();
+Console.Clear();
+Console.Title =" fag tilmeldning app";
+Console.WriteLine("-----------------------------------------------");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("{1},{2} {0}", Console.Title,skole,hovedforløb);
+Console.WriteLine("-----------------------------------------------");
+Console.ReadKey();
+
