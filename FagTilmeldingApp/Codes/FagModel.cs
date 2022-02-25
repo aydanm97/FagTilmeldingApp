0dad54dc3a5dc0417c725351795a852ae66cb1fe
aydanm97 <aydanm97@outlook.dk>
2022-02-25 12:05:12 +0100

    init

---
+++ FagTilmeldingApp/Codes/FagModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FagTilmeldingApp.Codes
+{
+    internal class FagModel: IComparable<FagModel>
+    {
+        public int CourseID { get; set; }
+        public string? CourseName { get; set; }
+        public int CompareTo(FagModel next)
+        {
+            return this.CourseName.CompareTo(next.CourseName);
+        }
+        //Sort by numbers
+
+        //public int CompareTo(FagModel next)
+        //{
+        //    return this.CourseID.CompareTo(next.CourseID);
+        //}
+    }
+}
--- FagTilmeldingApp/Program.cs
+++ FagTilmeldingApp/Program.cs
@@ -1,16 +1,48 @@
 ﻿using FagTilmeldingApp.Codes;
-// Iteration 2
-Semester se = new Semester("");
-Console.WriteLine("Angiv skole: ");
-string? skole = Console.ReadLine();
-Console.WriteLine("Angiv hovedforløb: ");
-string? hovedforløb = Console.ReadLine();
-Console.ReadKey();
-Console.Clear();
-Console.Title =" fag tilmeldning app";
-Console.WriteLine("-----------------------------------------------");
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("{1},{2} {0}", Console.Title,skole,hovedforløb);
-Console.WriteLine("-----------------------------------------------");
-Console.ReadKey();
+// Iteration 7_Icomparable
 
+
+List<FagModel> ListCourse = new()
+{
+    new FagModel() { CourseID = 1, CourseName = "Grundlæggende programmering" },
+    new FagModel() { CourseID = 2, CourseName = "Database programmering", },
+    new FagModel() { CourseID = 3, CourseName = "Studieteknik" }
+
+};
+Console.WriteLine("------------------------------------------");
+Console.WriteLine("Usorteret list af fag :");
+Console.WriteLine("------------------------------------------");
+foreach (FagModel course in ListCourse)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(course.CourseID + " "+course.CourseName);
+    Console.ForegroundColor= ConsoleColor.White;
+}
+Console.WriteLine("------------------------------------------");
+
+Console.WriteLine("Sorteret List af fag: ");
+Console.WriteLine("------------------------------------------");
+ListCourse.Sort();
+foreach (FagModel course1 in ListCourse)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(course1.CourseID + " " + course1.CourseName);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+Console.WriteLine("------------------------------------------");
+
+Console.WriteLine("Reverse listen af fag: ");
+Console.WriteLine("------------------------------------------");
+
+ListCourse.Reverse();
+
+foreach (FagModel course2 in ListCourse)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+   Console.WriteLine(course2.CourseID + ". " + course2.CourseName);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+
+
+Console.ReadKey();
